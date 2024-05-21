0714c5b7f34b88c5baa7c3898f8b57f1ea4e76c6
npetersen <npetersen@goldpointsystems.com>
2024-05-21 15:23:02 -0600

    Update Links and QR Code

--- public/js/script.js
+++ public/js/script.js
@@ -2,5 +2,5 @@ function copyLink() {
      
     /* Copy text into clipboard */
     navigator.clipboard.writeText
-        ("https://n8petersen.gitlab.io/ukrainehelplist/");
+        ("https://helpukraine.n8pete.com");
 }
